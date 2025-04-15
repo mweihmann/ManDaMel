4407fe60d2ada0a59bbfe53441def1322d5ab94f
MWeihmann <m@weihmann.at>
2025-04-15 14:08:29 +0200

    add dbml for the future db and JWT basics

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// set_error_handler('ErrorHandler::handleError');
+// set_exception_handler('ErrorHandler::handleException');
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
--- frontend/index.php
+++ frontend/index.php
@@ -29,5 +29,27 @@
 <body>
   <h1>Users</h1>
   <ul id="userList"></ul>
+
+
+  <div class="container">
+    <h2>User Registration</h2>
+    <form action="register.php" method="post">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+      </div>
+      <div class="form-group">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Register">
+      </div>
+    </form>
+  </div>
 </body>
 </html>
