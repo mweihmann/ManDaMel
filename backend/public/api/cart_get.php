88ca6c852dd06541b77ea2279852eb07112e9b35
MWeihmann <m@weihmann.at>
2025-05-17 14:01:39 +0200

    fix retrieving cart after login

--- backend/public/api/cart_get.php
+++ backend/public/api/cart_get.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Authorization, Content-Type");
 header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Content-Type: application/json");
